e3f55d676a7a42ee59011113431a5fab51cb1b78
Harish <harishprabakaranpro@gmail.com>
2024-10-30 10:25:57 +0100

    Update Fenetre.java

--- Jeu de la vie/Fenetre.java
+++ Jeu de la vie/Fenetre.java
@@ -12,8 +12,20 @@ public class Fenetre extends JFrame {
         setPreferredSize(new Dimension(700,700));
         setLocation(MAXIMIZED_HORIZ, MAXIMIZED_VERT);
 
+        Panel_principale panelA = new Panel_principale();
+        panelA.setLayout(new FlowLayout());
+
+        JButton nv_jeu = new JButton("Nouveau jeu");
+        
+        Nouv_partie nvl_partie = new Nouv_partie(nv_jeu);
+        nv_jeu.addActionListener(nvl_partie);
+
+        panelA.add(nv_jeu);
+
         jeu = new LogiqueDuJeu(getHeight()/10, getWidth()/10);
         g1 = new Grille(this, jeu);
-        add(g1);
+
+
+        panelA.add(g1);
     }
 }
